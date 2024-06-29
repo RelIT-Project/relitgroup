5e4dd2e654a832c49062439b1841b9cc67fb6602
Germán Silveira <146758565+Daldanos@users.noreply.github.com>
2024-06-29 21:19:03 +0000

    Add functions

--- script.js
+++ script.js
@@ -1,58 +1,54 @@
-// Slideshow script
-let slideIndex = 0;
+// Slideshow
+var slideIndex = 0;
 showSlides();
 
 function showSlides() {
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
     }
     slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    
-    for (let i = 0; i < dots.length; i++) {
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    setTimeout(showSlides, 4000); // Change image every 4 seconds
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Cambia la imagen cada 2 segundos
 }
 
-// Lightbox script
-let lightbox = document.getElementById('lightbox');
-let lightboxImg = document.getElementById('lightbox-img');
+// Lightbox
+var lightbox = document.getElementById("lightbox");
+var lightboxImg = document.getElementById("lightbox-img");
 
-function openLightbox(imageSrc) {
-    lightbox.style.display = 'block';
-    lightboxImg.src = imageSrc;
+function openLightbox(imgSrc) {
+    lightbox.style.display = "block";
+    lightboxImg.src = imgSrc;
 }
 
 function closeLightbox() {
-    lightbox.style.display = 'none';
+    lightbox.style.display = "none";
 }
 
-// Change image in lightbox
-let slideIndexLightbox = 1;
+// Cambia las imágenes en el lightbox
+var slideIndexLightbox = 1;
 
 function changeImage(n) {
     showImage(slideIndexLightbox += n);
 }
 
-function showImage(n) {
-    let slides = document.getElementsByClassName("mySlides");
-    
-    if (n > slides.length) { slideIndexLightbox = 1 }
-    if (n < 1) { slideIndexLightbox = slides.length }
-    
-    lightboxImg.src = slides[slideIndexLightbox - 1].querySelector('img').src;
+function currentImage(n) {
+    showImage(slideIndexLightbox = n);
 }
 
-// Automatically close lightbox if clicked outside the image
-window.onclick = function(event) {
-    if (event.target == lightbox) {
-        closeLightbox();
+function showImage(n) {
+    var images = document.getElementsByClassName("lightbox-image");
+    if (n > images.length) {slideIndexLightbox = 1}
+    if (n < 1) {slideIndexLightbox = images.length}
+    for (var i = 0; i < images.length; i++) {
+        images[i].style.display = "none";
     }
-};
+    images[slideIndexLightbox - 1].style.display = "block";
+}
